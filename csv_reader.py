c977d8f115d0651d9f108dd51254eb97c9824327
Rakesh Tiwari <rranjantiwari@gmail.com>
2022-12-02 12:42:16 +0530

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,7 +1,6 @@
 import csv
 #open the csv file
-#path should be full directory path ex: /home/username/directory/filename.csv
-with open('full-directory-path/users.csv', mode='r') as csv_file:
+with open('users.csv', mode='r') as csv_file:
     #read the CSV file
     csv_reader = csv.reader(csv_file)
     #read csv using DictReader class to show data in dictionary
